3f994c663cd64f02a7c4c6fa23db827c936eab33
kirvala13 <74554775+kirvala13@users.noreply.github.com>
2022-03-02 00:06:40 +0400

    bootcamp

--- src/components/personal-info/Personal.js
+++ src/components/personal-info/Personal.js
@@ -1,18 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Personal.css"
 import previus from "../../assets/Previous.png"
 import next from "../../assets/Next.png"
 import { Link } from 'react-router-dom'
 function Personal() {
+  const[name,setName]=useState('')
+  const[lastName,setLastName]=useState('')
+  const[email,setEmail]=useState('')
+  const[number,setNumber]=useState('')
+  const[valid,setValid]=useState(false)
+
+  const sentObj={
+    name:name,
+    lastName:lastName,
+    email:email,
+    number:number
+  }
+  if (localStorage.getItem("data") === null) {
+    localStorage.setItem("data", "[]")
+  }
+useEffect(()=>{
+  if(name.length<=2 || lastName.length<=3){
+    setValid(true)
+  }else{
+    setValid(false)
+  }
+},[name,lastName])
+  const saveClick=()=>{
+      const data=sentObj;
+      const oldData = JSON.parse(localStorage.getItem("data"))
+      oldData.push(data)
+      localStorage.setItem('data', JSON.stringify(oldData))
+  }
+
   return (
     <div className='personal__container'>
       <div className='cordinates-container'>
         <h1>Hey, Rocketeer, what are your coordinates?</h1>
         <form className='personal-form__container'>
-          <input type="text" placeholder='First name' />
-          <input type="text" placeholder='Last name' />
-          <input type="mail" placeholder='E-Mail' />
-          <input type="number" placeholder='+995 5_ _ _ _ _' />
+          <input type="text" placeholder='First name' onChange={(e)=>setName(e.target.value)} />
+          {valid?<div className='err'>*Not Valid Name</div>:null}
+          <input type="text" placeholder='Last name' onChange={(e)=>setLastName(e.target.value)}/>
+          {valid?<div className='err'>*Not Valid Last Name</div>:null}
+          <input type="mail" placeholder='E-Mail' onChange={(e)=>setEmail(e.target.value)} />
+          <input type="number" placeholder='+995 5_ _ _ _ _' onChange={(e)=>setNumber(e.target.value)}/>
           <div className='pagination'>
            <Link to="/"><img src={previus}/></Link>
             <div className='first-page' alt="prevbtn"></div>
@@ -21,11 +52,14 @@ function Personal() {
             <div></div>
             <div></div>
             <div></div>
-           <Link to="2"><img src={next} alt="nextbtn"/></Link>
+           <Link to="2" onClick={saveClick}><img src={next} alt="nextbtn"/></Link>
           </div>
         </form>
       </div>
-      <div className='origins-container'></div>
+      <div className='origins-container'>
+        <h1>Redberry Origins</h1>
+        <p>You watch “What? Where? When?” Yeah. Our founders used to play it. That’s where they got a question about a famous American author and screenwriter Ray Bradbury. Albeit, our CEO Gaga Darsalia forgot the exact name and he answered Ray Redberry. And at that moment, a name for a yet to be born company was inspired - Redberry 😇</p>
+      </div>
     </div>
   )
 }
--- src/components/technical/Technical.js
+++ src/components/technical/Technical.js
@@ -1,8 +1,79 @@
-import React from 'react'
-
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import "./Technical.css"
+import previus from "../../assets/Previous.png"
+import next from "../../assets/Next.png"
+import { Link } from 'react-router-dom'
+import removeImg from "../../assets/Remove.png"
 function Technical() {
+  const [skills, setSkills] = useState([]);
+  const [experience, setExperience] = useState('')
+  const [language, setLanguage] = useState("")
+
+  if (localStorage.getItem("tech") === null) {
+    localStorage.setItem("tech", "[]")
+  }
+
+  const TechClick = (e, i) => {
+    const newObj = {
+      experience: experience,
+      language: language
+    }
+    const tech = newObj;
+    const techdata = JSON.parse(localStorage.getItem("tech"))
+    techdata.push(tech)
+    localStorage.setItem('tech', JSON.stringify(techdata))
+    window.location.reload()
+  }
+  const technicalList = JSON.parse(localStorage.getItem("tech"))
+  const deleteSkill = (index) => {
+    window.location.reload()
+    const splic = technicalList.splice(index, 1)
+    localStorage.setItem('tech', JSON.stringify(technicalList))
+  }
+  useEffect(() => {
+    axios.get("https://bootcamp-2022.devtest.ge/api/skills")
+      .then(res => {
+        setSkills(res.data)
+      })
+  }, [])
   return (
-    <div>Technical</div>
+    <div className='personal__container'>
+      <div className='cordinates-container'>
+        <h1>Tell us about your skills</h1>
+        <select className="skills" onChange={(e) => setLanguage(e.target.value)}>
+          <option value="skills">skills</option>
+          {skills.map(res => {
+            return <option key={res.id} value={res.title}>{res.title}</option>
+          })}
+        </select>
+        <input type="text" placeholder='Experience Duration in Years' onChange={(e) => setExperience(e.target.value)} />
+        <div className='add-programming__language' onClick={TechClick}>Add Programming Languages</div>
+        <div className='skills-table'>
+          {
+            technicalList.map((res, index) => {
+              return <div key={index} className='technical__list'>
+                <p>{res.language}</p> <p>{`Years of Experiance: ${res.experience}`}</p> <img onClick={() => deleteSkill(index)} src={removeImg} />
+              </div>
+            })
+          }
+        </div>
+      </div>
+      <div className='pagination'>
+        <Link to="1"><img src={previus} /></Link>
+        <Link to="1"><div className='first-page' alt="prevbtn"></div></Link>
+        <div className='first-page'></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <Link to="3" ><img src={next} alt="nextbtn" /></Link>
+      </div>
+      <div className='origins-container'>
+        <h1>A bit about our battles</h1>
+        <p>As we said, Redberry has been on the field for quite some time now, and we have touched and embraced a variety of programming languages, technologies, philosophies, and frameworks. We are battle-tested in PHP Laravel Stack with Vue.js, refined in React, and allies with Serverside technologies like Docker and Kubernetes, and now we have set foot in the web3 industry.</p>
+      </div>
+    </div>
   )
 }
 
